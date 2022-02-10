3f56980e3ef4dcd6a1db16844268443b7ee2363d
Marcelo Paez Sequeira <paezao@gmail.com>
2022-02-10 13:29:37 -0300

    Added antd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import About from "./pages/About";
+import './App.less';
+import { Layout, Menu, Typography, Row, Col } from "antd";
+
+const { Header, Content, Footer } = Layout;
+const { Link, Text, Paragraph } = Typography;
+
+interface MenuInfo {
+    key: string;
+    keyPath: string[];
+    item: React.ReactInstance;
+    domEvent: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>;
+}
 
 function App() {
+  let navigate = useNavigate();
+
+  const handleMenuClick = ({ key }: MenuInfo) => {
+    navigate(key)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header className="header">
+        <Menu mode="horizontal" onClick={handleMenuClick} className="menu">
+          <Menu.Item key="/" style={{marginLeft: "auto"}}>About</Menu.Item>
+          <Menu.Item key="/experience">Experience</Menu.Item>
+          <Menu.Item key="/contact">Contact</Menu.Item>
+        </Menu>
+      </Header>
+      <Content className="content">
+        <Routes>
+          <Route path="/" element={<About />} />
+        </Routes>
+      </Content>
+      <Footer className="footer">
+        <Row justify="center" align="middle">
+          <Col>
+            <Paragraph>
+              <Text>Created by myself</Text>
+            </Paragraph>
+            <Link href="https://github.com/paezao/website">This website is opensource</Link>
+          </Col>
+        </Row>
+      </Footer>
+    </Layout>
   );
 }
 
---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,13 @@
+import { Row, Col } from "antd";
+
+const About = () => {
+  return (
+    <Row>
+      <Col>
+        <h1>Marcelo Paez Sequeira</h1>
+      </Col>
+    </Row>
+  )
+}
+
+export default About;
